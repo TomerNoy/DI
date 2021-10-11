2ace527a53b59b412b833a1e024bc225a7f52803
TomerNoy <mail2tomer@gmail.com>
2021-10-11 20:24:25 +0300

    .

---
+++ week2/day2/ninja.js
@@ -0,0 +1,10 @@
+// exercise 1:
+// const bDate1 = 1979, bDate2 = 2000;
+
+// const age1 = 2021 - Number(bDate1), age2 = 2021 - Number(bDate2);
+// const val = age2 / age1;
+// const result = val === 0.5 ? 'bDate2' : val === 2 ? 'bDate1' : null;
+// console.log(`age1 = ${age1}, age2 = ${age2} result = ${result}`);
+//------------------------------------------------
+
+
