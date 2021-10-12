5438a0c741c27f68e0464cc857c7a9843257f13c
TomerNoy <mail2tomer@gmail.com>
2021-10-12 23:42:00 +0300

    .

--- week2/day3/daily-challenge.js
+++ week2/day3/daily-challenge.js
@@ -8,3 +8,25 @@
 //     while (val.length <= x) val += '*';
 //     console.log(val);
 // }
+//------------------------------------------------------
+
+//##
+//## gold
+//##
+
+// const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+// const numbersString = numbers.toString();
+// //------------------------------------------------------
+// const numbersJoin = numbers.join('-');
+// //------------------------------------------------------
+// for (let x = 0; x < numbers.length; x++) {
+//     for (let y = 0; y < numbers.length; y++) {
+//         if (numbers[y] < numbers[y + 1]) {
+//             let temp = numbers[y];
+//             numbers[y] = numbers[y + 1];
+//             numbers[y + 1] = temp;
+//         }
+//     }
+// }
+
+// console.log(numbers);
