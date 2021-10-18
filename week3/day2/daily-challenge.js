011be77e15dffa26d3b954a439f7a253c303fb5c
TomerNoy <mail2tomer@gmail.com>
2021-10-18 20:49:18 +0300

    .

--- week3/day2/daily-challenge.js
+++ week3/day2/daily-challenge.js
@@ -1,15 +1,3 @@
-
-// Get the value of each of the inputs in the HTML file when the button is clicked.
-
-// Write a story that uses each of the values.
-// Make sure you check the console for errors when playing the game.
-
-
-// Bonus: Add a “shuffle” button to the HTML file, 
-//      when clicked the button should change the story currently displayed 
-//      (but keep the values entered by the user). The user could click the 
-//      button at least three times and get a new story. Display the stories randomly.
-
 let button = document.getElementById('lib-button');
 button.addEventListener('click', genStory);
 
