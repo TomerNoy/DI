229f458baa968d94c591f86e6f24e5256e3d6568
TomerNoy <mail2tomer@gmail.com>
2021-11-02 10:48:02 +0200

    changed week5 day2 challenge

--- week5/day2/challenge.py
+++ week5/day2/challenge.py
@@ -1,46 +1,56 @@
+# this exercise was done in class
+
 import random
+
 
 class Gene:
     def __init__(self):
-        self.state = random.randint(0,1)
+        self.state = random.randint(0, 1)
 
     def flip(self):
         self.state = 0 if self.state == 1 else 1
 
+    def __repr__(self):
+        return f'{self.state}'
+
+
 class Chromosome:
     def __init__(self):
-        self.genes = [Gene() for i in range(0,10)]
+        self.genes = [Gene() for i in range(0, 10)]
         print('created 10 genes')
         print(self.genes)
-        
+
     def mutate(self):
-        indexs = list(range(0,len(self.genes)))
-        random.shuffle(indexs)
-        print('shuffled indexes', indexs)
-        index_sub_selection = indexs[0:random.choice(indexs)]
+        indexes = list(range(0, len(self.genes)))
+        random.shuffle(indexes)
+        print('shuffled indexes', indexes)
+        index_sub_selection = indexes[0:random.choice(indexes)]
         print('the shuffled subselection', index_sub_selection)
         for idx in index_sub_selection:
             print('looping over gene', idx)
 
-            if random.randint(0,1):
+            if random.randint(0, 1):
                 self.genes[idx].flip()
                 print('i flipped', self.genes[idx])
             else:
-                print('i didnt flip')
+                print('i didn\'t flip')
+
 
 class DNA:
     def __init__(self):
         self.chromosomes = [Chromosome() for i in range(10)]
-    
+
     def mutate(self):
-        selected_chromosomes = random.sample(self.chromosomes, random.randint(0, len(self.chromosomes)))
+        selected_chromosomes = random.sample(
+            self.chromosomes, random.randint(0, len(self.chromosomes)))
         for chromo in selected_chromosomes:
-            if random.randint(0,1):
-                chromo.mutate()  
+            if random.randint(0, 1):
+                chromo.mutate()
                 print('i mutated', chromo)
             else:
-                print('i didnt mutated')
+                print('i didn\'t mutate')
 
 
 chromo = Chromosome()
-chromo.mutate() 
+chromo.mutate()
+print(chromo.genes)
