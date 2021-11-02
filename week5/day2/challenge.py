23255463e3a4bc085ea06c4747bf6740d35938e0
TomerNoy <mail2tomer@gmail.com>
2021-11-02 10:19:46 +0200

    week5 day2 challenge from class

--- week5/day2/challenge.py
+++ week5/day2/challenge.py
@@ -1,77 +1,46 @@
 import random
 
+class Gene:
+    def __init__(self):
+        self.state = random.randint(0,1)
 
-class Gene():
-    genes = []
+    def flip(self):
+        self.state = 0 if self.state == 1 else 1
 
-    def __init__(self, val):
-        self.genes.append(val)
-
+class Chromosome:
+    def __init__(self):
+        self.genes = [Gene() for i in range(0,10)]
+        print('created 10 genes')
+        print(self.genes)
+        
     def mutate(self):
-        sequence = ''
-        for i in range(len(self.genes)):
-            self.genes[i] = random.randint(0, 1)
-            sequence += str(self.genes[i])
-        print(sequence)
+        indexs = list(range(0,len(self.genes)))
+        random.shuffle(indexs)
+        print('shuffled indexes', indexs)
+        index_sub_selection = indexs[0:random.choice(indexs)]
+        print('the shuffled subselection', index_sub_selection)
+        for idx in index_sub_selection:
+            print('looping over gene', idx)
+
+            if random.randint(0,1):
+                self.genes[idx].flip()
+                print('i flipped', self.genes[idx])
+            else:
+                print('i didnt flip')
+
+class DNA:
+    def __init__(self):
+        self.chromosomes = [Chromosome() for i in range(10)]
+    
+    def mutate(self):
+        selected_chromosomes = random.sample(self.chromosomes, random.randint(0, len(self.chromosomes)))
+        for chromo in selected_chromosomes:
+            if random.randint(0,1):
+                chromo.mutate()  
+                print('i mutated', chromo)
+            else:
+                print('i didnt mutated')
 
 
-class Chromosome(Gene):
-    def __init__(self, genes):
-        for g in genes:
-            super().__init__(g)
-
-
-class DNA(Chromosome):
-    def __init__(self, chromosomes):
-        for c in chromosomes:
-            super().__init__(c)
-
-
-class Organism:
-    # environment is the percentage number
-    # to be in the range of a random number from 0 -100
-    def __init__(self, dna, environment):
-        ran = random.randint(0, 100)
-        if ran <= environment:
-            dna.mutate()
-
-# generates random dna sequence
-
-
-def DNA_generator():
-    dna = []
-    for c in range(10):
-        # creating a single chromosom
-        chromosome = []
-        for j in range(10):
-
-            gene = Gene(random.randint(0, 1))
-            chromosome.append(gene)
-
-        dna.append(chromosome)
-    return DNA(dna)
-
-
-yeast_dna = DNA_generator()
-print(yeast_dna.genes)
-
-# algae = DNA_generator()
-# protozoa = DNA_generator()
-
-# yeast = Organism(yeast_dna, 20)
-# algae = Organism(algae, 45)
-# protozoa = Organism(protozoa, 90)
-
-
-# # generates random dna sequence
-# def DNA_generator():
-#     chromosomes = []
-#     # create 10 chromosome for dna
-#     for c in range(10):
-#         genes = []
-#         # create 10 genes for each chromosome
-#         for g in range(10):
-#             gene = Gene(random.randint(0, 1))
-#             genes.append(gene)
-#         chromosomes.append(Chromosome(genes))
-#     return DNA(chromosomes)
+chromo = Chromosome()
+chromo.mutate() 
